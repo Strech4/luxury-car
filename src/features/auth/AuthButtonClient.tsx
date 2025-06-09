1833a12203aa1c3458167d2479a0598f717147b1
Stretch4 <88911439+Strech4@users.noreply.github.com>
2025-06-09 13:14:30 +0200

    Refactor AuthButtonClient to use async user fetching
    - Changed AuthButtonClient to an async function for user retrieval.
    - Replaced session check with user check from getUser function.

--- src/features/auth/AuthButtonClient.tsx
+++ src/features/auth/AuthButtonClient.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import React from 'react'
 import { SignInButton } from './SignInButton'
 import { useSession } from '@/lib/auth-client'
@@ -7,11 +5,11 @@ import { getUser } from '@/lib/auth-session'
 import Link from 'next/link'
 import { LoggedInButton } from './LoggedInButton'
 
-export const AuthButtonClient = () => {
+export const AuthButtonClient = async () => {
 
-    const { data: session, } = useSession()
+    const user = await getUser()
 
-    if (session?.user) {
+    if (user) {
         return <LoggedInButton />
     }
     return <SignInButton />
